81c08293a77f2d0b8897ceb4447c692bd4580651
jennymarie1027 <jennymarie1027@gmail.com>
2024-12-23 16:43:46 -0700

    delete completed items functionality

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+    def trash_em
+        @trash_em = List.where(:completed => true).destroy_all
+        redirect_to lists_url, notice: "All marked items were successfully removed."
+    end
+end
