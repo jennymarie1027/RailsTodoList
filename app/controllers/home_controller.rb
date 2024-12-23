dcce832f61c4b94cbe4aefd527dc2aa09148356a
jennymarie1027 <jennymarie1027@gmail.com>
2024-12-23 16:49:16 -0700

    delete all items functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,9 @@ class HomeController < ApplicationController
         @trash_em = List.where(:completed => true).destroy_all
         redirect_to lists_url, notice: "All marked items were successfully removed."
     end
+
+    def trash_all
+        @trash_all = List.all.destroy_all
+        redirect_to lists_url, notice: "All items were successfully removed."
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :lists
   root to: 'lists#index'
   get 'home/trash_em'
+  get 'home/trash_all'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
